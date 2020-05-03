75f86ad8aa340baac579ab600c93da0591b5389f
Lucas Rodrigues <lucasrb18@gmail.com>
2020-05-03 02:47:17 -0300

    Connecting to mongoDB with repository and service

---
+++ src/main/java/com/onlyme/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.onlyme.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.onlyme.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
---
+++ src/main/java/com/onlyme/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.onlyme.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.onlyme.workshopmongo.domain.User;
+import com.onlyme.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+
+}
